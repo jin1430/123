c1ec2271a8bd12ebcbb6b12a5e681ef046309873
HsH1121 <eju200149@gmail.com>
2025-08-28 08:34:19 +0900

    new readme file

---
+++ GoCafe/src/main/java/com/example/GoCafe/GoCafeApplication.java
@@ -0,0 +1,13 @@
+package com.example.GoCafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoCafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoCafeApplication.class, args);
+	}
+
+}
