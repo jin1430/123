4c172defc2cb72b78ca1c9ef08188ffce631e949
HsH1121 <eju200149@gmail.com>
2025-08-28 18:32:32 +0900

    User

---
+++ GoCafe/src/main/java/com/example/GoCafe/dto/UserForm.java
@@ -0,0 +1,38 @@
+// src/main/java/com/example/GoCafe/dto/UserForm.java
+package com.example.GoCafe.dto;
+
+import com.example.GoCafe.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserForm {
+
+    private Long userId;
+    private String userEmail;
+    private String userPassword;
+    private String userNickname;
+    private Long userAge;
+    private String userGender;
+    private String userRole;
+    private java.time.LocalDateTime userDate;
+    private String userPhoto;
+
+    public User toEntity() {
+        return new User(
+                userId,
+                userEmail,
+                userPassword,
+                userNickname,
+                userAge,
+                userGender,
+                userRole,
+                userDate,
+                userPhoto
+        );
+    }
+}
